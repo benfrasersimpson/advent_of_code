a40ca076dacbef7a1a67bed988e99452fbccf000
Ben Simpson <benfrasersimpson@gmail.com>
2022-06-06 09:58:30 +0100

    add is_straight_line for line

--- 2021/day_five/src/lib.rs
+++ 2021/day_five/src/lib.rs
@@ -13,13 +13,13 @@ pub enum ParsingError {
 
 #[cfg(test)]
 mod tests {
-    use crate::{coords_to_line, Point};
+    use crate::{Line, Point};
 
     #[test]
     fn test_coords_to_line() {
         let coords = "0,9 -> 5,9";
 
-        let result = coords_to_line(coords);
+        let result = Line::from(coords);
 
         assert_eq!(
             result,
@@ -28,6 +28,21 @@ mod tests {
                 end: Point { x: 5, y: 9 }
             })
         );
+    }
+
+    #[test]
+    fn test_is_straight_line() {
+        assert!((Line {
+            start: Point { x: 0, y: 9 },
+            end: Point { x: 5, y: 9 }
+        })
+        .is_straight_line());
+
+        assert!(!(Line {
+            start: Point { x: 0, y: 10 },
+            end: Point { x: 5, y: 8 }
+        })
+        .is_straight_line());
     }
 }
 
@@ -45,6 +60,10 @@ impl Line {
         let end = Point::from(end)?;
 
         Ok(Line { start, end })
+    }
+
+    fn is_straight_line(&self) -> bool {
+        self.start.x == self.end.x || self.start.y == self.end.y
     }
 }
 
